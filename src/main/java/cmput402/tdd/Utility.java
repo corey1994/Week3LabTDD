f55abc21ecb0b1c47e54db4d58803c830b3aa45b
corey hunt <hunt1@ualberta.ca>
2019-02-06 18:26:14 -0700

    Added TDD stuff

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -1,19 +1,46 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cmput402.tdd.service.Search;
 
 public class Utility {
 
-	public HashMap<String, Integer>  countAverageScore(Search search){
+	public HashMap<String, Float>  countAverageScore(Search search){
 		
-		return null;
+		HashMap<String, ArrayList<Integer>> results = search.returnAll();
+		
+		HashMap<String, Float> map = new HashMap<String, Float>();
+		
+		for(String player: results.keySet()) {
+			ArrayList<Integer> playerStats = results.get(player);
+			Integer numGames = playerStats.get(0);
+			Integer numGoals = playerStats.get(1);
+			
+			Float average = (float) numGoals / numGames;
+			map.put(player, average);
+		}
+		return map;
 	}
 	
-public HashMap<String, Integer>  findTopScorer(Search search){
-		
-		return null;
-	}
-	
+//public HashMap<String, Integer>  findTopScorer(Search search){
+//		
+//	HashMap<String, ArrayList<Integer>> stats = search.returnAll();
+//	
+//	HashMap<String, Integer> map = new HashMap<String, Integer>();
+//	String bestPlayer = new String();
+//	
+//	for(String player: stats.keySet()) {
+//		if(bestPlayer == null) {
+//			bestPlayer = player;
+//			
+//		} else if(stats.get(player).get(1) > stats.get(bestPlayer).get(1)) {
+//			bestPlayer = player;
+//		}
+//	}
+//	map.put(bestPlayer, stats.get(bestPlayer).get(1));
+//	return map;
+//	}
+//	
 }
